a7770d2aab417ec28894306637dd00b7fb95edec
Adam-Wallenrod <psredniawa@gmail.com>
2018-05-25 00:55:42 +0200

    54) adding shopping list edit section

--- recipe-book/src/app/recipes/recipe-list/recipe-list.component.ts
+++ recipe-book/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,7 +8,8 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
-    new Recipe('A test recipe', 'This is simply test', 'https://www.maxpixel.net/static/photo/2x/Meat-Power-Recipe-Food-Dishes-Pork-1459693.jpg')
+    new Recipe('A test recipe', 'This is simply test', 'https://www.maxpixel.net/static/photo/2x/Meat-Power-Recipe-Food-Dishes-Pork-1459693.jpg'),
+    new Recipe('A test recipe2', 'This is simply test2', 'https://www.maxpixel.net/static/photo/2x/Meat-Power-Recipe-Food-Dishes-Pork-1459693.jpg')
   ];
 
   constructor() { }
