b9f343e9b5338466aa8b03373c213cafeef57cd3
Adam-Wallenrod <psredniawa@gmail.com>
2018-05-24 22:52:50 +0200

    48) adding content to the Recipes Components

---
+++ recipe-book/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A test recipe', 'This is simply test', 'https://www.maxpixel.net/static/photo/2x/Meat-Power-Recipe-Food-Dishes-Pork-1459693.jpg')
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
