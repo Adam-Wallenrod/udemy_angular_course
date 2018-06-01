56536b50befc63f8e1fdaa424803ec265f187d71
Adam-Wallenrod <psredniawa@gmail.com>
2018-06-01 17:56:33 +0200

    158) Project cleaning

--- recipe-book/src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ recipe-book/src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Recipe } from '../recipe.model';
 import { RecipeService  } from '../recipe.service';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -12,8 +12,10 @@ export class RecipeDetailComponent implements OnInit {
   recipe: Recipe;
   id: number;
 
-  constructor(private recipeService: RecipeService,
-               private route: ActivatedRoute
+
+  constructor( private recipeService: RecipeService,
+               private route: ActivatedRoute,
+               private router: Router
              ) { }
 
   ngOnInit() {
@@ -29,6 +31,11 @@ export class RecipeDetailComponent implements OnInit {
 
   onAddToShopingList(){
     this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients);
+  }
+
+  onEditRecipe(){
+    //this.router.navigate(['edit'], {relativeTo: this.route});
+    this.router.navigate(['recipes', this.id, 'edit']);
   }
 
 }
