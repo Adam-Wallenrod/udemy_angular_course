c660de6e6db7309a4595a7ead05a5a847e261aef
Adam-Wallenrod <psredniawa@gmail.com>
2018-07-15 23:17:09 +0200

    finished Template Driven forms

--- forms-td-start/src/app/app.component.ts
+++ forms-td-start/src/app/app.component.ts
@@ -7,10 +7,36 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  @ViewChild('f') signupForm: NgForm
+  @ViewChild('f') signupForm: NgForm;
+  defaultQuestion= 'pet' ;
+  answer = '';
+  genders = ['male', 'female'];
+  submitted = false;
+
+  user = {
+    username: '',
+    email: '',
+    secretQuestion: '',
+    answer: '',
+    gender: ''
+  };
+
+
 
   suggestUserName() {
     const suggestedName = 'Superuser';
+    // this.signupForm.setValue({
+    //   userData: {username: suggestedName, email:''},
+    //   secret: 'pet',
+    //   questionAswer: '',
+    //   gender: 'male'
+    // });
+
+    this.signupForm.form.patchValue({
+      userData: {
+        username: suggestedName
+      }
+    });
   }
 
   // onSubmit(form: NgForm) {
@@ -19,7 +45,14 @@ export class AppComponent {
   // }
 
   onSubmit(){
-      console.log('Submitted (alternative method)!');
+      this.submitted = true;
+      // console.log('Submitted (alternative method)!');
+      this.user.username = this.signupForm.value.userData.username;
+      this.user.email = this.signupForm.value.userData.email;
+      this.user.secretQuestion = this.signupForm.value.secret;
+      this.user.answer = this.signupForm.value.questionAnswer;
+      this.user.gender = this.signupForm.value.gender;
       console.log(this.signupForm);
+      this.signupForm.reset();
   }
 }
