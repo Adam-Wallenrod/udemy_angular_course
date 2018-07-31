9fe76b8f0a1d0d509df57a00de1d0d5a09cb27b6
Adam-Wallenrod <psredniawa@gmail.com>
2018-07-31 23:06:06 +0200

    working route transition animation

--- playground/src/app/app.module.ts
+++ playground/src/app/app.module.ts
@@ -1,13 +1,12 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 
 import { CockpitComponent } from './cockpit/cockpit.component';
-import { FormsModule } from '@angular/forms';
-import { RouterModule, Routes } from '@angular/router';
-
-import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
-
 import { MemoryAComponent } from './memory-a/memory-a.component';
 import { MemoryBComponent } from './memory-b/memory-b.component';
 import { MemoryCComponent } from './memory-c/memory-c.component';
--- playground/src/app/fade.animation.ts
+++ playground/src/app/fade.animation.ts
@@ -51,7 +51,7 @@ export const fadeAnimation =
       query(':leave',
         [
           style({ opacity: 1 }),
-          animate('0.2s', style({ opacity: 0 }))
+          animate('0.4s', style({ opacity: 0 }))
         ],
         { optional: true }
       ),
@@ -59,7 +59,7 @@ export const fadeAnimation =
       query(':enter',
         [
           style({ opacity: 0 }),
-          animate('0.2s', style({ opacity: 1 }))
+          animate('0.4s', style({ opacity: 1 }))
         ],
         { optional: true }
       )
