d88d1c6838b70731d0a61ccf0d86c58385fd4b70
Adam-Wallenrod <psredniawa@gmail.com>
2018-08-09 00:36:13 +0200

    added methods for Line Series Chart (errors with getColor, scaleType)

--- playground/src/app/custom-chart/custom-chart.component.ts
+++ playground/src/app/custom-chart/custom-chart.component.ts
@@ -1,45 +1,60 @@
-import { Component, Input, OnChanges } from '@angular/core';
-import { BaseChartComponent, ColorHelper } from '@swimlane/ngx-charts'
+import {Component, Input, OnChanges} from '@angular/core';
+import {
+  BaseChartComponent,
+  ColorHelper,
+  LineComponent,
+  LineSeriesComponent
+} from '@swimlane/ngx-charts';
 import * as d3 from 'd3';
+import {area, line, curveLinear} from 'd3-shape';
+import {scaleBand, scaleLinear, scalePoint, scaleTime} from 'd3-scale';
 
 @Component({
   selector: 'app-custom-chart',
-  //templateUrl: './custom-chart.component.html',
-  template: `<ngx-charts-chart
-    [view]="[width, height]"
-    [showLegend]="false"
-    style="padding:60px;">
-    <svg:g ngx-charts-x-axis
-           [xScale]="xScale"
-           [dims]="dims"
-           [showLabel]="true"
-           [labelText]="'X values'"
-    >
-    </svg:g>
-    <svg:g ngx-charts-y-axis
-           [yScale]="yScale"
-           [dims]="dims"
-           [showGridLines]="true"
-           [showLabel]="true"
-           [labelText]="'Y values'"
-           [tickFormatting]="yAxisTickFormatting"
-           (dimensionsChanged)="updateYAxisWidth($event)">
-    </svg:g>
-    <svg:g class="bar-chart chart">
-      <svg:g
-        ngx-charts-series-vertical
-        [xScale]="xScale"
-        [yScale]="yScale"
-        [colors]="colors"
-        [series]="results"
-        [dims]="dims">
+  template: `
+    <ngx-charts-chart
+      [view]="[width, height]"
+      [showLegend]="false"
+      style="padding:60px;">
+      <svg:g ngx-charts-x-axis
+             [xScale]="xScale"
+             [dims]="dims"
+             [showLabel]="true"
+             [labelText]="'X values'"
+      >
       </svg:g>
-    </svg:g>
-  </ngx-charts-chart>`,
+      <svg:g ngx-charts-y-axis
+             [yScale]="yScale"
+             [dims]="dims"
+             [showGridLines]="true"
+             [showLabel]="true"
+             [labelText]="'Y values'">
+      </svg:g>
+      <svg:g class="bar-chart chart">
+        <svg:g
+          ngx-charts-series-vertical
+          [xScale]="xScale"
+          [yScale]="yScale"
+          [colors]="colors"
+          [series]="results"
+          [dims]="dims">
+        </svg:g>
+      </svg:g>
+      <svg:g *ngFor="let series of lineChart; trackBy:trackBy">
+        <svg:g ngx-charts-line-series
+               [xScale]="xScaleLine"
+               [yScale]="yScaleLine"
+               [colors]="colorsLine"
+               [data]="series"
+               [scaleType]="'linear'"
+               [curve]="curve"/>
+      </svg:g>
+    </ngx-charts-chart>`,
   styleUrls: ['./custom-chart.component.css']
 })
 export class CustomChartComponent extends BaseChartComponent
   implements OnChanges {
+
 
   dims: any;
   xScale: any;
@@ -49,9 +64,25 @@ export class CustomChartComponent extends BaseChartComponent
   colors: ColorHelper;
   colorScheme: any = 'cool';
 
+
+  // key inputs
   @Input() view;
   @Input() results;
+  @Input() lineChart: any;
+  @Input() curve: any = curveLinear;
+  @Input() ready = false;
 
+
+  // line chart prop
+  xScaleLine;
+  yScaleLine;
+  xDomainLine;
+  yDomainLine;
+  seriesDomain;
+  colorsLine: ColorHelper;
+  scaleType = 'linear';
+  transform: string; /// ???
+  bandwidth; /// ???
 
 
   ngOnChanges() {
@@ -67,6 +98,19 @@ export class CustomChartComponent extends BaseChartComponent
     this.xScale = this.getXScale();
     this.yScale = this.getYScale();
     this.setColors();
+
+    // line chart
+    this.xDomainLine = this.getXDomainLine();
+    this.yDomainLine = this.getYDomainLine();
+
+    //this.seriesDomain = this.getSeriesDomain();
+
+    this.xScaleLine = this.getXScaleLine(this.xDomainLine, this.dims.width);
+    this.yScaleLine = this.getYScaleLine(this.yDomainLine, this.dims.height);
+  }
+
+  trackBy(index, item): string {
+    return item.name;
   }
 
   getXScale() {
@@ -77,6 +121,7 @@ export class CustomChartComponent extends BaseChartComponent
       .paddingInner(spacing)
       .domain(this.xDomain);
   }
+
   getYScale() {
     this.yDomain = this.getYDomain();
     return d3.scaleLinear()
@@ -87,14 +132,138 @@ export class CustomChartComponent extends BaseChartComponent
   getXDomain() {
     return this.results.map(d => d.name);
   }
+
   getYDomain() {
     let values = this.results.map(d => d.value);
     let min = Math.min(0, ...values);
     let max = Math.max(...values);
     return [min, max];
   }
+
   setColors() {
     this.colors = new ColorHelper(this.colorScheme, 'ordinal', this.xDomain);
+  }
+
+
+  // line chart
+  getScaleType(values): string {
+    let date = true;
+    let num = true;
+
+    for (const value of values) {
+      // if (!this.isDate(value)) {
+      //   date = false;
+      // }
+
+      if (typeof value !== 'number') {
+        num = false;
+      }
+    }
+
+    if (date) {
+      return 'time';
+    }
+    if (num) return 'linear';
+    return 'ordinal';
+  }
+
+  getXDomainLine(): any[] {
+    let values = [];
+
+    for (const results of this.lineChart) {
+      for (const d of results.series) {
+        if (!values.includes(d.name)) {
+          values.push(d.name);
+        }
+      }
+    }
+
+    this.scaleType = this.getScaleType(values);
+    let domain = [];
+
+    if (this.scaleType === 'time') {
+      const min = Math.min(...values);
+      const max = Math.max(...values);
+      domain = [min, max];
+    } else if (this.scaleType === 'linear') {
+      values = values.map(v => Number(v));
+      const min = Math.min(...values);
+      const max = Math.max(...values);
+      domain = [min, max];
+    } else {
+      domain = values;
+    }
+    // this.xSet = values;
+    return domain;
+  }
+
+
+  getYDomainLine(): any[] {
+    const domain = [];
+
+    for (const results of this.lineChart) {
+      for (const d of results.series) {
+        if (domain.indexOf(d.value) < 0) {
+          domain.push(d.value);
+        }
+        if (d.min !== undefined) {
+          if (domain.indexOf(d.min) < 0) {
+            domain.push(d.min);
+          }
+        }
+        if (d.max !== undefined) {
+          if (domain.indexOf(d.max) < 0) {
+            domain.push(d.max);
+          }
+        }
+      }
+    }
+
+    let min = Math.min(...domain);
+    const max = Math.max(...domain);
+    // if (this.yRightAxisScaleFactor) {
+    //   const minMax = this.yRightAxisScaleFactor(min, max);
+    //   return [Math.min(0, minMax.min), minMax.max];
+    // } else {
+    min = Math.min(0, min);
+    return [min, max];
+    // }
+  }
+
+  getXScaleLine(domain, width): any {
+    let scale;
+    if (this.bandwidth === undefined) {
+      // this.bandwidth = (this.dims.width - this.barPadding);
+      this.bandwidth = this.dims.width;
+    }
+
+    if (this.scaleType === 'time') {
+      scale = scaleTime()
+        .range([0, width])
+        .domain(domain);
+    } else if (this.scaleType === 'linear') {
+      scale = scaleLinear()
+        .range([0, width])
+        .domain(domain);
+
+      //  if (this.roundDomains) {
+      //    scale = scale.nice();
+      //  }
+    } else if (this.scaleType === 'ordinal') {
+      scale = scalePoint()
+        .range([this.bandwidth / 2, width - this.bandwidth / 2])
+        .domain(domain);
+    }
+
+    return scale;
+  }
+
+  getYScaleLine(domain, height): any {
+    const scale = scaleLinear()
+      .range([height, 0])
+      .domain(domain);
+
+    return scale;
   }
 }
 
--- playground/src/app/test-chart/test-chart.component.ts
+++ playground/src/app/test-chart/test-chart.component.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations'
 import { NgxChartsModule } from '@swimlane/ngx-charts';
 import { single, multi } from '../data';
+import * as shape from 'd3-shape';
+import * as d3 from 'd3';
 
 
 @Component({
@@ -28,6 +30,132 @@ export class TestChartComponent {
   explodeSlices = false;
   doughnut = true;
 
+  // custom chart component
+  curves = {
+    Basis: shape.curveBasis,
+    Step: shape.curveStep,
+    'Step After': shape.curveStepAfter,
+    'Step Before': shape.curveStepBefore
+  };
+
+  // line interpolation
+  curveType: string = 'Step';
+  curve: any = this.curves[this.curveType];
+  interpolationTypes = [
+    'Basis',
+    'Step',
+    'Step After',
+    'Step Before'
+  ];
+
+
+  data = [
+    {
+      'name': 'Germany',
+      'value': 46268
+    },
+    {
+      'name': 'USA',
+      'value': 53041
+    },
+    {
+      'name': 'France',
+      'value': 42503
+    },
+    {
+      'name': 'United Kingdom',
+      'value': 41787
+    },
+    {
+      'name': 'Spain',
+      'value': 29863
+    },
+    {
+      'name': 'Italy',
+      'value': 35925
+    }
+  ];
+
+  lineChartSeries = [
+    {
+      name: 'Tablets',
+      series: [
+        {
+          name: 'USA',
+          value: 50
+        },
+        {
+          value: 80,
+          name: 'United Kingdom'
+        },
+        {
+          value: 85,
+          name: 'France'
+        },
+        {
+          value: 90,
+          name: 'Japan'
+        },
+        {
+          value: 100,
+          name: 'China'
+        }
+      ]
+    },
+    {
+      name: 'Cell Phones',
+      series: [
+        {
+          value: 10,
+          name: 'USA'
+        },
+        {
+          value: 20,
+          name: 'United Kingdom'
+        },
+        {
+          value: 30,
+          name: 'France'
+        },
+        {
+          value: 40,
+          name: 'Japan'
+        },
+        {
+          value: 10,
+          name: 'China'
+        }
+      ]
+    },
+    {
+      name: 'Computers',
+      series: [
+        {
+          value: 2,
+          name: 'USA',
+
+        },
+        {
+          value: 4,
+          name: 'United Kingdom'
+        },
+        {
+          value: 20,
+          name: 'France'
+        },
+        {
+          value: 30,
+          name: 'Japan'
+        },
+        {
+          value: 35,
+          name: 'China'
+        }
+      ]
+    }
+  ];
+
+
   constructor() {
     Object.assign(this, {single, multi});
   }
@@ -35,33 +163,5 @@ export class TestChartComponent {
   onSelect(event) {
     console.log(event);
   }
-
-
-  data = [
-    {
-      "name": "Germany",
-      "value": 46268
-    },
-    {
-      "name": "USA",
-      "value": 53041
-    },
-    {
-      "name": "France",
-      "value": 42503
-    },
-    {
-      "name": "United Kingdom",
-      "value": 41787
-    },
-    {
-      "name": "Spain",
-      "value": 29863
-    },
-    {
-      "name": "Italy",
-      "value": 35925
-    }
-  ];
 
 }
