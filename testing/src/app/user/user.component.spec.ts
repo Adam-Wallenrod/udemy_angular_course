cbc6e621c518da08196b684f1fd3af21d13ffeff
Adam-Wallenrod <psredniawa@gmail.com>
2018-08-19 15:58:24 +0200

    unit tests part 2 - testing pipe

--- testing/src/app/user/user.component.spec.ts
+++ testing/src/app/user/user.component.spec.ts
@@ -79,28 +79,3 @@ describe('UserComponent', () => {
 
 
 });
-
-
-
-
-// describe('UserComponent', () => {
-//   let component: UserComponent;
-//   let fixture: ComponentFixture<UserComponent>;
-//
-//   beforeEach(async(() => {
-//     TestBed.configureTestingModule({
-//       declarations: [ UserComponent ]
-//     })
-//     .compileComponents();
-//   }));
-//
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(UserComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
-//
-//   it('should be created', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
