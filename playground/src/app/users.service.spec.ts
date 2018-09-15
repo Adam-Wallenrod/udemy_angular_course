9e35144cf133c6f1badc5d3d1f8a79656e891c8e
Adam-Wallenrod <psredniawa@gmail.com>
2018-09-15 16:46:38 +0200

    added test for users.service

--- playground/src/app/users.service.spec.ts
+++ playground/src/app/users.service.spec.ts
@@ -1,15 +1,70 @@
-import { TestBed, inject } from '@angular/core/testing';
+// import { TestBed, inject } from '@angular/core/testing';
+//
+// import { UsersService } from './users.service';
+//
+// describe('UsersService', () => {
+//   beforeEach(() => {
+//     TestBed.configureTestingModule({
+//       providers: [UsersService]
+//     });
+//   });
+//
+//   it('should be created', inject([UsersService], (service: UsersService) => {
+//     expect(service).toBeTruthy();
+//   }));
+// });
 
+import { TestBed, async, inject } from '@angular/core/testing';
+import { HttpClientModule } from '@angular/common/http';
+import {  Response,
+          ResponseOptions,
+          XHRBackend} from '@angular/http';
+import { MockBackend } from '@angular/http/testing';
 import { UsersService } from './users.service';
 
+
 describe('UsersService', () => {
+
   beforeEach(() => {
+
     TestBed.configureTestingModule({
-      providers: [UsersService]
+      imports: [HttpClientModule],
+      providers: [
+        UsersService,
+        { provide: XHRBackend, useClass: MockBackend },
+      ]
     });
   });
 
-  it('should be created', inject([UsersService], (service: UsersService) => {
-    expect(service).toBeTruthy();
-  }));
+  describe('getUsers()', () => {
+
+    it('should return an Observable<Array<Users>>',
+        inject([UsersService, XHRBackend], (usersService, mockBackend) => {
+
+        const mockResponse = {
+          data: [
+            { id: 0, name: 'Users 0' },
+            { id: 1, name: 'Users 1' },
+            { id: 2, name: 'Users 2' },
+            { id: 3, name: 'Users 3' },
+          ]
+        };
+
+        mockBackend.connections.subscribe((connection) => {
+          connection.mockRespond(new Response(new ResponseOptions({
+            body: JSON.stringify(mockResponse)
+          })));
+        });
+
+        usersService.getUsers().subscribe((users) => {
+          console.log(users);
+          expect(users.length).toBe(4);
+          expect(users[0].name).toEqual('Users 0');
+          expect(users[1].name).toEqual('Users 1');
+          expect(users[2].name).toEqual('Users 2');
+          expect(users[3].name).toEqual('Users 3');
+        });
+
+    }));
+  });
 });
