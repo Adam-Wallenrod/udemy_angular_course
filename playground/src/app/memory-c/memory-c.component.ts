7b3e52baf1006a9068194da9c921083afb9322cb
Adam-Wallenrod <psredniawa@gmail.com>
2019-01-27 00:19:12 +0100

    added datalist

--- playground/src/app/memory-c/memory-c.component.ts
+++ playground/src/app/memory-c/memory-c.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+
+interface IBrowser {
+  id: number;
+  name: string;
+}
+
 
 @Component({
   selector: 'app-memory-c',
@@ -6,10 +12,53 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./memory-c.component.css']
 })
 export class MemoryCComponent implements OnInit {
+  @ViewChild('inputRef') inputRef: any;
+
+  availableBrowsers: IBrowser[] = [
+    { id: 1, name: 'Chrome' },
+    { id: 2, name: 'Firefox' },
+    { id: 3, name: 'Internet Explorer' },
+    { id: 4, name: 'Opera' },
+    { id: 5, name: 'Safari' }];
+
+  disableCreateBtn: boolean = true;
+  selectedBrowser: IBrowser = { name: 'none', id: 1000 };
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSubmitClick(form: any) {
+    console.log('Form: ', form.value);
+
+  }
+
+  onCreateClick() {
+    let result = prompt("Enter id value: ");
+    let newId = parseInt(result);
+    console.log('prompt result: ', newId);
+    this.selectedBrowser = { name: 'aaa', id: newId }
+    console.log('this.inputRef: ', this.inputRef);
+  }
+
+  checkText(text) {
+    console.log('text: ', text);
+    this.disableCreateBtn = false;
+
+    if (text === '') {
+      this.disableCreateBtn = true;
+    }
+    else {
+      for (let browser of this.availableBrowsers) {
+        if (text === browser.name) {
+          this.disableCreateBtn = true;
+          break;
+        }
+      }
+    }
+
+
   }
 
 }
