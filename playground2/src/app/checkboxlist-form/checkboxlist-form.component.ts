22518d71b7835c89621e05abb28d899a38b8c4e0
Adam-Wallenrod <psredniawa@gmail.com>
2020-05-03 18:20:28 +0200

    added concatMap improving issues with checkboxlist-form

--- playground2/src/app/checkboxlist-form/checkboxlist-form.component.ts
+++ playground2/src/app/checkboxlist-form/checkboxlist-form.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {MulticheckboxListSelectionChangeEv, OptionType} from '../checkboxlist/checkboxlist.component';
 import {FormControl, FormGroup} from '@angular/forms';
+import {Observable, of, Subject} from 'rxjs';
+import {concatMap} from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-checkboxlist-form',
@@ -8,6 +11,8 @@ import {FormControl, FormGroup} from '@angular/forms';
   styleUrls: ['./checkboxlist-form.component.scss']
 })
 export class CheckboxlistFormComponent implements OnInit {
+
+  private selectionSubject = new Subject<MulticheckboxListSelectionChangeEv>();
 
   form = new FormGroup({
     selectedItems: new FormControl('')
@@ -17,36 +22,66 @@ export class CheckboxlistFormComponent implements OnInit {
   checked = false;
 
   checkboxListOptions: OptionType[] = [
-    {value: 1, label: "option_1"},
-    {value: 2, label: "option_2"},
-    {value: 3, label: "option_3"}
+    {value: 1, label: 'option_1'},
+    {value: 2, label: 'option_2'},
+    {value: 3, label: 'option_3'}
   ];
 
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
+
+    this.selectionSubject
+      .pipe(concatMap(data => {
+
+        const id = data.option.value;
+
+        if (data.checkboxEvent.checked) {
+
+          return Observable.create((observer) => {
+            const timeout = setTimeout(() => {
+              console.log('Returning data!');
+
+              observer.next({
+                type: 'ADD',
+                data: data.option
+              });
+
+              observer.complete();
+            }, 1500);
+
+            return () => clearTimeout(timeout);
+
+          });
+
+        } else {
+
+          return of({
+            type: 'REMOVE',
+            data: id
+          });
+        }
+
+      }))
+      .subscribe((action: any) => {
+
+        console.log('action - ', action.type);
+        if(action.type === 'ADD') {
+          this.records.push(action.data);
+        } else {
+          this.records = this.records.filter( x => x.value !== action.data);
+        }
+
+
+      });
+
   }
 
 
-
   onCheckListChange(data: MulticheckboxListSelectionChangeEv) {
-
-    const id = data.option.value;
-
-    if ( data.checkboxEvent.checked === true ) {
-      setTimeout(
-        () => {
-          this.records.push(data.option);
-        }, 1500
-      );
-
-
-    } else {
-
-      this.records = this.records.filter(x => x.value !== id);
-    }
-
+    this.selectionSubject.next(data);
   }
 
 }
